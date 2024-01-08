3edc5ffa01499d7548cb1f777094b8cb4fb1786a
Azat Kashapov <azat.developer@gmail.com>
2024-01-08 10:51:38 +0100

    remove storage dir env

--- src/plugins/static.ts
+++ src/plugins/static.ts
@@ -1,10 +1,8 @@
 import fp from 'fastify-plugin';
 import { fastifyStatic } from '@fastify/static';
-import path from 'path';
 
 export default fp(async (fastify) => {
-  const __dirname = process.env.STORAGE_DIR as string
   fastify.register(fastifyStatic, {
-    root: path.join(__dirname, '/storage'),
+    root: '/app/storage',
   });
 });
